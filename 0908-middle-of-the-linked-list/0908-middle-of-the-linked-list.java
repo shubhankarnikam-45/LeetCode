30687d1699696aab405a4f7db272c713b4f29a3e
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-06-04 10:37:02 +0530

    Time: 0 ms (100%), Space: 40.5 MB (94.42%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -19,13 +19,14 @@ class Solution {
         {
             //loop break condition 1.
             if(fast.next == null) break;
-            
+
             slow = slow.next;
             //loop break condition 2
             if(fast.next.next == null) break;
             fast = fast.next.next;
         }
         //return slow pointer that points to the middle of linked list.
+        //return the answer.
         return slow;
     }
 }
