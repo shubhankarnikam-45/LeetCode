ad25443fdfc30c478adb6b8faa9b7771e9e52018
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-07-08 19:44:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (13.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,40 +1,44 @@
 class Solution {
-    public int search(int[] arr, int target) {
+    public int search(int[] arr, int key) {
+         //define the low and high.
+        int low = 0;
+        int high = arr.length-1;
         
-         int low = 0;
-            int high = arr.length-1;
-
         while(low <= high)
+        {
+            //middle element.
+            int mid = (low + (high- low)/2);  //to avoid overflow
+            
+            //if elemnt is found.
+            if(arr[mid] == key) return mid;
+            
+            //left part is sorted or not ?
+            if(arr[low] <= arr[mid])
             {
-                //mid .
-                int mid = (low + (high-low)/2);
-
-                if(arr[mid] == target) return mid;
-
-                if(arr[low] <= arr[mid])
+                if(key >= arr[low] && key<=arr[mid])
                 {
-                        if(arr[low] <= target && target <= arr[mid])
-                        {
-                            high = mid-1;
-                        }
-                        else
-                        {
-                            low = mid + 1;
-                        }
+                    high = mid - 1;
                 }
                 else
                 {
-                    if(arr[mid] <= target && target<= arr[high])
-                    {
-                        low = mid + 1;
-                    }
-                    else
-                    {
-                        high = mid -1;
-                    }
+                    low = mid + 1;
                 }
             }
-
+            //right part is sorted or not ?
+            else if(arr[mid]<=arr[high])
+            {
+                if(key >= arr[mid] && key<=arr[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid -1;
+                }
+            }
+        }
+        
+        //if not answer found then we return -1;
         return -1;
     }
 }
