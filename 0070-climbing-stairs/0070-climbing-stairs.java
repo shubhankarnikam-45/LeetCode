5026bb3dce3c7d24ba287129b2d802a6a8812718
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-07-20 18:55:14 +0530

    Time: 0 ms (100%), Space: 40.3 MB (28.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,5 @@
 class Solution {
-    public int climbStairsHelper(int n, int dp[]) {
-        
-        if(n == 0) return 1;
-        if(n == 1) return 1;
-
-        if(dp[n] != 0) return dp[n];
-
-        dp[n] = climbStairsHelper(n-1, dp) + climbStairsHelper(n-2,dp);
-        return dp[n];
-    }
+    
 
     public int climbStairs(int n) {
         
@@ -16,7 +7,12 @@ class Solution {
         dp[0] = 1;
         dp[1] = 1;
 
-        return climbStairsHelper(n, dp);
+        for(int i=2; i<=n ;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
         
         
     }
