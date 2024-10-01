7467750a83c8719cb32613a67bb973d33e1fa93c
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-10-01 11:06:09 +0530

    Time: 41 ms (66.64%), Space: 55.3 MB (32.13%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,39 +1,10 @@
 class Solution {
-    public int fun(int left, int right, String s, int dp[][])
-    {
-        //base case.
-        if(left == right) return 1;
-        if(left > right) return 0;
-
-        if(dp[left][right] != -1) return dp[left][right];
-
-        if(s.charAt(left) == s.charAt(right))
-        {
-            return dp[left][right] = 2 + fun(left + 1, right - 1, s, dp);
-        }
-        
-        return dp[left][right] = Math.max(fun(left + 1, right, s, dp), fun(left, right - 1, s, dp));
-
-
-    }
+   
     public int longestPalindromeSubseq(String s) {
         
         int m = s.length();
         int dp[][] = new int[m][m];
-        // for(int arr[] : dp) Arrays.fill(arr, -1);
-
-        // return fun(0, m - 1, s, dp); 
-
-
-        //TABULATION.
-        // for(int i=0; i<m ;i++)
-        // {
-        //     for(int j=0; j<m; j++)
-        //     {
-        //         if(i == j) dp[i][j] = 1;
-        //     }
-        // }
-
+       
 
         for(int left = (m - 1); left >= 0; left--)
         {
