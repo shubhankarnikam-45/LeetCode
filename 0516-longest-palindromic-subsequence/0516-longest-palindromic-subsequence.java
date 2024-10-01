f690d99c1908638273f8c87d42627267d1e9531b
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-10-01 10:23:36 +0530

    Time: 43 ms (63.93%), Space: 55.7 MB (6.97%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -20,8 +20,41 @@ class Solution {
         
         int m = s.length();
         int dp[][] = new int[m][m];
-        for(int arr[] : dp) Arrays.fill(arr, -1);
+        // for(int arr[] : dp) Arrays.fill(arr, -1);
 
-        return fun(0, m - 1, s, dp); 
+        // return fun(0, m - 1, s, dp); 
+
+
+        //TABULATION.
+        // for(int i=0; i<m ;i++)
+        // {
+        //     for(int j=0; j<m; j++)
+        //     {
+        //         if(i == j) dp[i][j] = 1;
+        //     }
+        // }
+
+
+        for(int left = (m - 1); left >= 0; left--)
+        {
+            for(int right = 0; right <m; right++)
+            {
+
+                if(left == right) dp[left][right] = 1;
+                else if(left > right) dp[left][right] = 0;
+                else
+                {
+                        if(s.charAt(left) == s.charAt(right))
+                        {
+                                dp[left][right] = 2 + dp[left + 1] [right - 1];
+                        }
+                        else
+                        dp[left][right] = Math.max(dp[left + 1] [right] , dp[left][right - 1] );
+                }
+
+            }
+        }
+
+        return dp[0][m - 1];
     }
 }
