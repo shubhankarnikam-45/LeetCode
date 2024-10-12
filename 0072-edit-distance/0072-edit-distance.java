4464e89ebc86bd3441038e1dfd6803f9e3f99f3f
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-10-12 16:19:56 +0530

    Time: 5 ms (65.18%), Space: 44.9 MB (49.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -22,11 +22,29 @@ return dp[ind1][ind2] = 1 + Math.min(fun(ind1 , ind2 - 1, s, t , dp), Math.min(f
         int n = s.length();
         int m = t.length();
 
-        int dp[][] = new int[n][m];
-        for(int arr[] : dp) Arrays.fill(arr, -1);
+        int dp[][] = new int[n + 1][m + 1];
+        
+        //first we wriet down the base cases.
+        for(int ind1 = 1; ind1 <=n; ind1++) dp[ind1][0] = ind1;
+        for(int ind2 = 1; ind2 <=m; ind2++) dp[0][ind2] = ind2;
 
-        // if(n == 0) return m;
+        //tabulation.
+        for(int ind1 = 1; ind1 <=n; ind1++)
+        {
+            for(int ind2 = 1; ind2<=m; ind2++)
+            {
+                //if character matches.
+                if(s.charAt(ind1 - 1) == t.charAt(ind2 - 1))
+                {
+                    dp[ind1][ind2] = dp[ind1 - 1][ind2 - 1];
+                }
+                else
+                {
+                    dp[ind1][ind2] =  1 + Math.min(dp[ind1][ind2-1], Math.min(dp[ind1 - 1][ind2 - 1], dp[ind1 -1 ][ind2]));
+                }
+            }
+        }
 
-        return fun(n - 1, m - 1, s, t,dp);
+        return dp[n][m];
     }
 }
