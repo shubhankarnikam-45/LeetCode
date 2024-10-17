40a787d1acd2b609c9b0d67dfe685a1e58f8e46d
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-10-17 13:10:48 +0530

    Time: 76 ms (37.12%), Space: 64.2 MB (35.63%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,5 @@
 class Solution {
-    public int fun(int ind, int buy, int cnt, int prices[], int dp[][][])
-    {
-        //base case.
-        if(ind == prices.length) return 0;
-        if(cnt == 2) return 0;
-
-        //if answe already exists.
-        if(dp[ind][buy][cnt] != -1) return dp[ind][buy][cnt];
-
-        int profit = 0;
-
-        if(buy == 1) //buy
-        {
-            profit = Math.max(-prices[ind] + fun(ind + 1, 0, cnt , prices, dp), 0 + fun(ind + 1, 1, cnt, prices, dp));   
-        }
-        else //sell
-        {
-            profit = Math.max(prices[ind] + fun(ind + 1, 1, cnt + 1, prices, dp), 0 + fun(ind + 1, 0, cnt, prices, dp));
-        }
-
-        return dp[ind][buy][cnt] = profit;
-    }
+    
     public int maxProfit(int[] prices) {
         
         int n = prices.length;
