cdfd9e3e9536e4f1bfc3212537c73ba2141dd1de
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2025-03-21 09:17:01 +0530

    Time: 1 ms (99.84%), Space: 46.3 MB (35.45%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int XOR = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            XOR ^= nums[i];
+        }
+        
+        return XOR;
+    }
+}
