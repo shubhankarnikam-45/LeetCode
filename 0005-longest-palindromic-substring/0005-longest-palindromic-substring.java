3f0c02944297fcaf8ffb3b96ca531ea2e82dc2ca
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2025-09-20 11:44:31 +0530

    Time: 417 ms (19.4%), Space: 56.5 MB (5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,33 @@
 class Solution {
 
-    public boolean checkPalindrome(int i, int j, String s)
+    public int checkPalindrome(int i, int j, String s, int dp[][])
     {
-        while(i < j)
-        {
-            if(s.charAt(i) == s.charAt(j)){
+        //base case.
+        if(i > j || i == j) return 1;
 
-                i++;
-                j--;
-            }
-            else return false;
-        }
+        if(dp[i][j] != -1) return dp[i][j];
 
-        return true;
+        if(s.charAt(i) == s.charAt(j)) 
+            return dp[i][j] = checkPalindrome(i+1, j-1, s, dp);
+
+        return dp[i][j] =  0;
     }
     public String longestPalindrome(String s) {
         
         //length of string.
         int n = s.length();
         
+        int dp[][] = new int[1001][1001];
+
+        for(int arr[] : dp) Arrays.fill(arr, -1);
+
         //the answer string we later return.
         String ans = "";
         for(int i=0; i<n; i++)
         {
             for(int j=i; j<n; j++)
             {
-                if(checkPalindrome(i, j, s) == true)
+                if(checkPalindrome(i, j, s, dp) == 1)
                 {
                     String candidate = s.substring(i, j + 1);
                     
